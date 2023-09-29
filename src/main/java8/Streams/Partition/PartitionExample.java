0ce017c62b16a5b2b096eccfd63a177a0f80ca8f
chitchan <chitirala.chandan_kumar@noka.com>
2023-09-29 23:57:10 +0530

    streams

---
+++ src/main/java8/Streams/Partition/PartitionExample.java
@@ -0,0 +1,23 @@
+package Streams.Partition;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PartitionExample {
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1,2,8,6,5,0,7);
+
+        // •	Partition a list of numbers into even and odd numbers using partitioningBy.
+        System.out.println(numbers.stream().collect(Collectors.partitioningBy(n -> n%2 == 0)));
+
+
+    }
+
+
+
+
+
+
+}
