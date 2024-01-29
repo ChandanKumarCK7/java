3220d069e676991504b5e610722d1eb32389d3d8
ChandanKumarCK7 <chandan.ck7.3899@gmail.com>
2024-01-29 15:19:51 +0530

    threads

--- src/main/java8/FunctionalInterface/ThreadCounterWithFunctionalInterfaceAndWithoutFunctionalInterface.java
+++ src/main/java8/FunctionalInterface/ThreadCounterWithFunctionalInterfaceAndWithoutFunctionalInterface.java
@@ -13,12 +13,27 @@ class Pojo{
         return v;
     }
 
-    public void setV(int v) {
+    public synchronized void setV(int v) {
         this.v = v;
     }
 }
-public class ThreadCounterWithFunctionalInterface {
-    public static void main(String[] args){
+
+class RunnableImpl implements Runnable{
+    Pojo p;
+    public RunnableImpl(Pojo p) {
+        this.p = p;
+    }
+
+    @Override
+    public void run(){
+        for(int i =0; i < 5; i++){
+            p.setV(p.getV()+1);
+            System.out.println(p.getV()+" "+Thread.currentThread().getName());
+        }
+    }
+}
+public class ThreadCounterWithFunctionalInterfaceAndWithoutFunctionalInterface {
+    public static void main(String[] args) throws InterruptedException {
         // sample Example to create a thread with lambda and functional interface and implement counter
 
         Pojo p = new Pojo(0);
@@ -37,6 +52,11 @@ public class ThreadCounterWithFunctionalInterface {
 
         t1.start();
         t2.start();
+
+        RunnableImpl r = new RunnableImpl(p);
+        Thread t8 = new Thread(r, "t8");
+
+        t8.start();
 
     }
 }
